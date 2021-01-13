5781899d7e20474210d529fd02b6b44eaf3a4d40
poojaanand3333 <poojaanand3333@gmail.com>
2021-01-13 12:37:38 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { TopicsComponent } from './topics/topics.component';
+import { Page1Component } from './page1/page1.component';
+import { MainpComponent } from './mainp/mainp.component';
+const appRoutes: Routes = [
+  { path: 'page1', component: Page1Component },
+  { path: 'home', component:TopicsComponent },
+  { path: 'main', component:MainpComponent },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopicsComponent,
+    Page1Component,
+    MainpComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/mainp/mainp.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector:'app-main',
+    templateUrl:'./mainp.component.html',
+    styleUrls: ['./mainp.component.css']
+})
+
+export class MainpComponent{
+
+}
